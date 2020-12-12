7069b4ffa0cf0bc342003c33d5ef676fbb8edd5e
ZhangZisu <i@zzs1.cn>
2020-12-12 21:21:56 +0800

    Update

--- src/web/oauth/github.ts
+++ src/web/oauth/github.ts
@@ -1,8 +1,15 @@
 import { FastifyPluginAsync } from 'fastify'
 import got from 'got'
 import { Db } from 'mongodb'
+import * as uuid from 'uuid'
 import { getCollections, getMetaValue } from '../../db'
-import { DI, K_DB, notNull, S_KEY_OAUTH_CONFIG } from '../../utils'
+import {
+  DI,
+  generatePasswordPair,
+  K_DB,
+  notNull,
+  S_KEY_OAUTH_CONFIG
+} from '../../utils'
 import { S, UserDTO } from '../common'
 
 export const githubOAuthPlugin: FastifyPluginAsync = async (V) => {
@@ -57,6 +64,49 @@ export const githubOAuthPlugin: FastifyPluginAsync = async (V) => {
         'oauth.github': userId
       })
       notNull(user)
+      const token = V.jwt.sign({ _id: user._id }, { expiresIn: expires })
+      return { user, token }
+    }
+  )
+
+  V.post(
+    '/register',
+    {
+      schema: {
+        body: S.object()
+          .prop('code', S.string().required())
+          .prop('state', S.string().required())
+          .prop('expires', S.enum(['1d']).default('1d')),
+        response: {
+          200: S.object().prop('user', UserDTO).prop('token', S.string())
+        }
+      }
+    },
+    async (req) => {
+      const {
+        body: { code, state, expires }
+      } = <any>req
+      const {
+        body: { access_token }
+      } = await got('https://github.com/login/oauth/access_token', {
+        searchParams: {
+          client_id,
+          client_secret,
+          code,
+          state
+        },
+        responseType: 'json'
+      })
+      const userInfo = await getUserInfo(access_token)
+      const r = await Users.insertOne({
+        slug: uuid.v4(),
+        name: userInfo.login,
+        email: userInfo.email,
+        pass: await generatePasswordPair(uuid.v4()),
+        perm: {},
+        oauth: { github: userInfo.id }
+      })
+      const user = r.ops[0]
       const token = V.jwt.sign({ _id: user._id }, { expiresIn: expires })
       return { user, token }
     }
